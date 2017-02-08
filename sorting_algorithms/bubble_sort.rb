f3181d7a307d97c81fcd96efcaace6fbcd90ce82
Sergei Mellow <sergei.mellow@gmail.com>
2017-02-08 00:13:15 -0500

    updated repo file structure and added bubble sort implementation

---
+++ sorting_algorithms/bubble_sort.rb
@@ -0,0 +1,57 @@
+# Bubble sort, sometimes referred to as sinking sort, is a simple sorting
+# algorithm that repeatedly steps through the list to be sorted, compares each
+# pair of adjacent items and swaps them if they are in the wrong order. The pass
+# through the list is repeated until no swaps are needed, which indicates that
+# the list is sorted. The algorithm, which is a comparison sort, is named for
+# the way smaller or larger elements "bubble" to the top of the list. Although
+# the algorithm is simple, it is too slow and impractical for most problems even
+# when compared to insertion sort.[1] It can be practical if the input is usually
+# in sorted order but may occasionally have some out-of-order elements nearly in
+# position.
+
+# Pseudocode implementation
+#The algorithm can be expressed as (0-based array):
+
+# procedure bubbleSort( A : list of sortable items )
+#    n = length(A)
+#    repeat 
+#      swapped = false
+#      for i = 1 to n-1 inclusive do
+#        /* if this pair is out of order */
+#        if A[i-1] > A[i] then
+#          /* swap them and remember something changed */
+#          swap( A[i-1], A[i] )
+#          swapped = true
+#        end if
+#      end for
+#    until not swapped
+# end procedure
+
+# Worst-case performance      O(n^{2})} O(n^{2})
+# Best-case performance	      O(n)} O(n)
+# Average performance	      O(n^{2})} O(n^{2})
+# Worst-case space complexity O(1)} O(1) auxiliary
+
+def bubble_sort array
+  n = array.length
+  swapped = true
+  while swapped == true
+    puts array.to_s
+    swapped = false
+    (1..n-1).each do |i|
+      if array[i-1] > array[i]
+        # swap( A[i-1], A[i] )
+        array[i-1], array[i] = array[i], array[i-1]
+        swapped = true
+      end
+    end
+  end
+  return array
+end
+
+arr = []
+# unsorted array of length 10 with random numbers between 0-99
+10.times{ arr << rand(100) }
+puts "unsorted array:" + arr.to_s + "\nsorting in progress..."
+arr = bubble_sort(arr)
+puts "done\n" + "sorted array:" + arr.to_s
---
+++ sorting_algorithms/quick_sort.rb
@@ -0,0 +1,94 @@
+# Quicksort (sometimes called partition-exchange sort) is an efficient sorting
+# algorithm, serving as a systematic method for placing the elements of an
+# array in order. Developed by Tony Hoare in 1959, with his work published
+# in 1961, it is still a commonly used algorithm for sorting. When implemented
+# well, it can be about two or three times faster than its main competitors,
+# merge sort and heapsort.  - Wikipedia
+###############################################################################
+# Algorithm: 
+# Quicksort is a divide and conquer algorithm. Quicksort first divides a large
+# array into two smaller sub-arrays: the low elements and the high elements.
+# Quicksort can then recursively sort the sub-arrays.
+###############################################################################
+# Algorithm steps:
+
+# 1) Pick an element, called a pivot, from the array.
+
+# 2) Partitioning: reorder the array so that all elements with values less than
+#    the pivot come before the pivot, while all elements with values greater
+#    than the pivot come after it (equal values can go either way). After this
+#    partitioning, the pivot is in its final position. This is called the
+#    partition operation.
+#
+# 3) Recursively apply the above steps to the sub-array of elements with
+#    smaller values and separately to the sub-array of elements with greater
+#    values.
+###############################################################################
+# The base case of the recursion is arrays of size zero or one, which never
+# need to be sorted.
+# The pivot selection and partitioning steps can be done in several different
+# ways; the choice of specific implementation schemes greatly affects the
+# algorithm's performance.
+
+# Pseudo Code:
+
+# algorithm quicksort(A, lo, hi) is
+#  if lo < hi then
+#        p := partition(A, lo, hi)
+#        quicksort(A, lo, p)
+#        quicksort(A, p + 1, hi)
+
+# algorithm partition(A, lo, hi) is
+#   pivot := A[hi]
+#   i := lo     // place for swapping
+#   for j := lo to hi – 1 do
+#     if A[j] ≤ pivot then
+#       swap A[i] with A[j]
+#       i := i + 1
+#   swap A[i] with A[hi]
+#   return i
+
+# Sorting the entire array is accomplished by quicksort(A, 1, length(A)).
+
+# Ruby implementation of quicksort
+require 'pry'
+require 'pry-byebug'
+def quicksort(array, lo, hi)
+  if lo < hi
+    p = partition(array, lo, hi)
+    quicksort(array, lo, p - 1)
+    quicksort(array, p + 1, hi)
+  end
+  return array 
+end
+
+def partition(array, lo, hi)
+  pivot = array[hi]
+  i = lo
+  (lo..hi-1).each_with_index do |j|
+    if array[j] <= pivot
+      # swap A[i] with A[j]
+      puts "loop1:" + array.to_s
+      array[i], array[j] = array[j], array[i]
+      puts "loop2:" + array.to_s
+      i = i + 1
+    end
+  end
+  # swap A[i] with A[hi]
+  puts "end 1" + array.to_s
+  array[i], array[hi] = array[hi], array[i]
+  puts "end 2" + array.to_s
+  return i
+end
+
+test_array_1 = [72, 5, 12, 1, 6]
+# print original array
+puts "Original Array:" 
+puts test_array_1.to_s
+puts "============="
+# solution
+binding.pry
+a = quicksort(test_array_1, 0, test_array_1.length - 1).to_s
+puts '============'
+puts "Sorted Array:"
+puts a
