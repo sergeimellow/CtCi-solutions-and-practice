a27789c798c5aed15e1c25a2a0d9d0c99d859c23
Sergei Mellow <sergei.mellow@gmail.com>
2017-04-01 17:44:56 -0400

    first pass at palindrome permutation and some placeholders

--- chapter_1/question_3_urlify.rb
+++ chapter_1/question_3_urlify.rb
@@ -2,6 +2,9 @@
 # has sufficient space at the end to hold the additional characters, and that you are given the "true"
 # length of the string. (Note: If implementing in Java, please use a character array so that you can
 # perform this operation in place.)
+# EXAMPLE
+# Input: "Mr John Smith    ", 13
+# Output: "Mr%20J ohn%20Smith"
 
 def urlify(str)
   str.strip.gsub(" ","%20")
--- chapter_1/question_4_palindrome_permutation.rb
+++ chapter_1/question_4_palindrome_permutation.rb
@@ -1,8 +1,17 @@
 # Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palin-
 # drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation
 # is a rea rrangement of letters. The palindrome does not need to be limited to just dictionary words.
+# EXAMPLE
+# Input: Tact Coa
+# Output: True (permutations: "taco cat". "atco cta". etc.)
 
 def palindrome_permutation str
-  #return false 
-  #str.downcase == str.reverse.downcase ? true : false
+  str_1 = str.chars.sort.join
+  str_2 = str.chars.sort.join.reverse
+  for i in 0..str.length/2
+    return false if str[i] != str[str.length-i-1]
+  end
+  return true
 end
+
+palindrome_permutation("Tact Coa")
