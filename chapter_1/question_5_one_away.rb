3ab3dfa6add52152a504d2de846eca43537964a7
Sergei Mellow <sergei.mellow@gmail.com>
2017-04-01 20:40:11 -0400

    first pass at question 5 chapter 5 one away

--- chapter_1/question_5_one_away.rb
+++ chapter_1/question_5_one_away.rb
@@ -7,5 +7,41 @@
 # pale, bale -> true
 # pale, bake -> false
 
-def one_away(str_one, str_two)
+def one_away(str_1, str_2)
+  str_1_length = str_1.length
+  str_2_length = str_2.length
+  # handles more than 1 character difference
+  return false if (str_1_length - str_2.length).abs > 1
+
+  # handles if strs already the same
+  return true if str_1 == str_2
+
+  if str_1_length >= str_2_length
+    larger_or_equal_length_str = str_1
+    smaller_or_equal_length_str = str_2
+  else
+    larger_or_equal_length_str = str_2
+    smaller_or_equal_length_str = str_1
+  end
+
+  # handles 1 character difference for both same length and 1 char diff
+  larger_or_equal_length_str.each_char.with_index do |char, index|
+    # handles replace
+    if str_1_length == str_2_length
+      temp_arr = smaller_or_equal_length_str.dup
+      temp_arr[index]=larger_or_equal_length_str[index]
+      return true if larger_or_equal_length_str == temp_arr
+    # handles remove / insert
+    else
+      temp_arr = smaller_or_equal_length_str.dup
+      temp_arr.insert(index, larger_or_equal_length_str[index])
+      return true if larger_or_equal_length_str == temp_arr
+    end
+  end
+  return false
 end
+
+puts one_away("pale", "ple")
+puts one_away("pales", "pale")
+puts one_away("pale", "bale")
+puts one_away("pale", "bake")
