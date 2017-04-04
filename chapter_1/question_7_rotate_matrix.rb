3bc1be85e237d0fc2f9e07536fac04b12ed94807
Sergei Mellow <sergei.mellow@gmail.com>
2017-04-03 21:57:09 -0400

    3rd pass at chapter 1 problem 7

--- chapter_1/question_7_rotate_matrix.rb
+++ chapter_1/question_7_rotate_matrix.rb
@@ -13,15 +13,22 @@ def rotate_matrix(n_by_n_matrix)
     v.times do |r| # r = rotation
       n_by_n_matrix[c][c+r], n_by_n_matrix[v+c-r][c] = n_by_n_matrix[v+c-r][c], n_by_n_matrix[c][c+r]
       n_by_n_matrix[v+c-r][c], n_by_n_matrix[v+c][v+c-r] = n_by_n_matrix[v+c][v+c-r], n_by_n_matrix[v+c-r][c]
-      n_by_n_matrix[v+c][v+c-r], n_by_n_matrix[c+r][v] = n_by_n_matrix[c+r][v], n_by_n_matrix[v+c][v+c-r]
+      n_by_n_matrix[v+c][v+c-r], n_by_n_matrix[c+r][v+c] = n_by_n_matrix[c+r][v+c], n_by_n_matrix[v+c][v+c-r]
     end
   end
   n_by_n_matrix
 end
 
 # 1 pixel = 4 bytes = 32 bits = 101010101...(etc)
-# using z,y,z as variable to show rotation
-n_by_n_matrix = [[0,1,2],[3,4,5],[6,7,8]]
+puts "Example 1, n=5"
+n_by_n_matrix = [[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24]]
+puts "before:"
+n_by_n_matrix.each{|x| puts x.to_s}
+rotated_n_by_n_matrix = rotate_matrix(n_by_n_matrix)
+puts "after:"
+rotated_n_by_n_matrix.each{|x| puts x.to_s}
+puts "Example 2. n=4"
+n_by_n_matrix = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]
 puts "before:"
 n_by_n_matrix.each{|x| puts x.to_s}
 rotated_n_by_n_matrix = rotate_matrix(n_by_n_matrix)
