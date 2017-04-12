254e39bb6c662d19d5c1ea925f1fa9fa5447c5a8
Sergei Mellow <sergei.mellow@gmail.com>
2017-04-11 21:53:59 -0400

    added follow up question solution using enumator that is o(n^2) time

--- chapter_2/question_1_remove_dups.rb
+++ chapter_2/question_1_remove_dups.rb
@@ -92,9 +92,21 @@ def remove_dups_with_buffer(linked_list)
 end
 
 # follow up question solution
-# def remove_dups_no_buffer(linked_list)
-#   linked_list
-# end
+def remove_dups_no_buffer(linked_list)
+  cur = linked_list.head;
+  while cur != nil
+    # Remove all future nodes that have the same value
+    enum = cur
+    while enum.next != nil do
+      if enum.next.data == cur.data
+        enum.next = enum.next.next
+      else
+       enum = enum.next
+      end
+    end
+    cur = cur.next
+  end
+end
 
 # test linked list data for initial solution
 test_input_1 = SinglyLinkedList.new(1)
@@ -104,20 +116,20 @@ test_input_1 = SinglyLinkedList.new(1)
 end
 
 # test linked list data for follow up solution
-# test_input_2 = test_input_1.dup
+test_input_2 = test_input_1.dup
 
 # initial problem
-puts "list before using buffer:"
-pp test_input_1
-test_input_1 = remove_dups_with_buffer(test_input_1)
-puts "list after using buffer:"
-pp test_input_1
+# puts "list before using buffer:"
+# pp test_input_1
+# test_input_1 = remove_dups_with_buffer(test_input_1)
+# puts "list after using buffer:"
+# pp test_input_1
 
 # follow up
-# puts "list before using no buffer:"
-# pp test_input_2
-# remove_dups_no_buffer(test_input_2)
-# puts "list after using no buffer:"
-# pp test_input_2
+puts "list before using no buffer:"
+pp test_input_2
+remove_dups_no_buffer(test_input_2)
+puts "list after using no buffer:"
+pp test_input_2
 
 
